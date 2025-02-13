b8eb467b7002cfe8ec4301119bbf72df0dc9bbac
Samuel Greco <s_greco@icloud.com>
2025-02-13 14:03:04 +0100

    [+] Add swagger self-documenting api to main program

--- BooksMinimalApi/Program.cs
+++ BooksMinimalApi/Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(app => {
+    app.SwaggerDoc("v1", new OpenApiInfo { Title = "Books Minimal API v1", Description = "Minimal Web API Project for books demo project.", Version = "v1" });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI(ui => {
+        ui.SwaggerEndpoint("/swagger/v1/swagger.json", "BooksMinimalApi v1");
+    });
+}
 
 app.MapGet("/", () => "Hello World!");
 
